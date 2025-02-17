f2355ea83de4e6c68f250eb0aab127f1434b104f
MGmetal <cemil.kahveci255@gmail.com>
2025-02-17 14:59:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ os.environ["DISPLAY"] = ":99"
 
 # Xvfb'nin çalıştığını kontrol et, yoksa başlat
 try:
-    xvfb_check = subprocess.run(["pgrep", "Xvfb"], capture_output=True, text=True)
-    if not xvfb_check.stdout.strip():
+    xvfb_check = subprocess.run(["ps", "aux"], capture_output=True, text=True)
+    if "Xvfb" not in xvfb_check.stdout:
         os.system("Xvfb :99 -ac -screen 0 1024x768x24 +extension GLX +render -noreset &")
 except Exception as e:
     print(f"Xvfb başlatılamadı: {e}")
